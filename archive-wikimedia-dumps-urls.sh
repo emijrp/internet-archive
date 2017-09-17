3bbac798752152e569ebc314f9ea2d4d7d03b9aa
emijrp <emijrp@gmail.com>
2017-09-17 22:05:15 +0200

    comments

--- archive-wikimedia-dumps-urls.sh
+++ archive-wikimedia-dumps-urls.sh
@@ -4,7 +4,7 @@ echo "https://dumps.wikimedia.org/mirrors.html" >> wikimedia-dumps-urls.txt
 python3 archive.py wikimedia-dumps-urls.txt force
 
 curl -A "Mozilla 5.0" "https://dumps.wikimedia.org/backup-index.html" 2> /dev/null | grep -ioE "[^/\"]+wiki/[0-9]+" | sort | uniq | awk '{split($0,a,"/"); print "https://dumps.wikimedia.org/"a[1]"/"a[2]"/"}' > wikimedia-dumps-urls.txt
-python3 archive.py wikimedia-dumps-urls.txt force
+python3 archive.py wikimedia-dumps-urls.txt force #force because sometimes dump is in progress
 
 curl -A "Mozilla 5.0" "https://dumps.wikimedia.org/backup-index.html" 2> /dev/null | grep -ioE "[^/\"]+wiki/[0-9]+" | sort | uniq | awk '{split($0,a,"/"); print "https://dumps.wikimedia.org/"a[1]"/"a[2]"/"a[1]"-"a[2]"-md5sums.txt\n""https://dumps.wikimedia.org/"a[1]"/"a[2]"/"a[1]"-"a[2]"-sha1sums.txt"}' > wikimedia-dumps-urls.txt
-python3 archive.py wikimedia-dumps-urls.txt
+python3 archive.py wikimedia-dumps-urls.txt #force not needed, just save once
