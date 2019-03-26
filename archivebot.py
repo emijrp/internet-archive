emijrp/internet-archive

commit 6cee2fe83428141b82cd914cee4c8fd501385c95

Sort transfer.sh URLs by filename instead of the random file ID

archivebot.py
@@ -45,6 +45,9 @@ def parselistline(line):
         url = url + '/'
     line = url + (' | ' + label if label else '')
     sorturl = truncationpattern.sub('', url).lower()
+    if sorturl.startswith('transfer.sh/') and sum(x == '/' for x in sorturl):
+        # For transfer.sh URLs that contain exactly two slashes, strip the first path component = the random file ID to sort by the filename instead.
+        sorturl = 'transfer.sh' + sorturl[sorturl.index('/', 12):]  # 12 = len('transfer.sh/')
     return Entry(sorturl = sorturl, url = url, label = label, line = line)
 
 def curateurls(wlist=''):

commit 7038ba457b6da32f164477400ef5145b363b2fa7

Fix bug in the slash check introduced in 6cee2fe8

archivebot.py
@@ -49,7 +49,7 @@ def parselistline(line):
         if domain == 'ix.io' and '+' not in sorturl:
             # Only apply this stripping to the undocumented trick URLs of format ix.io/code+/filename
             continue
-        if sorturl.startswith(domain) and sum(x == '/' for x in sorturl):
+        if sorturl.startswith(domain) and sum(x == '/' for x in sorturl) == 2:
             # For file hosting URLs that contain exactly two slashes, strip the first path component = the random file ID to sort by the filename instead.
             sorturl = domain + sorturl[sorturl.index('/', len(domain) + 1):]
     return Entry(sorturl = sorturl, url = url, label = label, line = line)